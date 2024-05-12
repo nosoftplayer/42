abbc6caf6c8e7ca0e6a30ac55a036c03aac726c6
nosoftplayer <mikayelyolchyan@gmail.com>
2024-05-12 18:34:18 +0400

    New

---
+++ Piscine/c00/ex06/ft_print_comb2.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_write_comb(int a, int b)
+{
+	ft_putchar(a / 10 + 48);
+	ft_putchar(a % 10 + 48);
+	ft_putchar(' ');
+	ft_putchar(b / 10 + 48);
+	ft_putchar(b % 10 + 48);
+	if (a != 98)
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_write_comb(a, b);
+			b++;
+		}
+		a++;
+	}
+}
+/*
+int main ()
+{
+	ft_print_comb2();
+}
+*/
---
+++ Piscine/c00/ex07/ft_putnbr.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
+/* int main()
+{
+	ft_putnbr(1234);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	ft_putnbr(5);
+	ft_putchar('\n');
+}*/
